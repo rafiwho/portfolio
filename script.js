7e7a24400f7691a0a8a47ea25b2d0a85fde64b7a
Md. Uodoy Hossan Rafi <144991940+rafiwho@users.noreply.github.com>
2025-05-27 14:04:06 +0600

    basic java script

---
+++ script.js
@@ -0,0 +1,11 @@
+function printing(){
+    let a = 5;
+    let b = 3;
+    let c = 'rafi';
+    const d = 3.1416
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(a + d);
+    console.log(Math.round(a / d));
+}
